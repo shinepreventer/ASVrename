d23c97ac45165daf02f86f87444c5d460144e469
shinepreventer <53377241+shinepreventer@users.noreply.github.com>
2023-09-21 14:35:03 +0800

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,5 +1,7 @@
 import pandas as pd
-from Bio import SeqIO
+from Bio import SeqIO, Phylo
+import tkinter as tk
+from tkinter import filedialog
 
 def write_single_line_fasta(records, output_file):
     with open(output_file, 'w') as f:
@@ -7,7 +9,7 @@ def write_single_line_fasta(records, output_file):
             f.write(f'>{record.id}\n')
             f.write(f'{record.seq}\n')
 
-def rename_asv_ids(asv_table_file, rep_seq_file, output_asv_table_file, output_rep_seq_file):
+def rename_asv_ids(asv_table_file, rep_seq_file, tree_file, output_asv_table_file, output_rep_seq_file, output_tree_file):
     # 读取ASV表
     asv_table = pd.read_csv(asv_table_file, sep='\t', index_col=0)
 
@@ -29,15 +31,46 @@ def rename_asv_ids(asv_table_file, rep_seq_file, output_asv_table_file, output_r
         record.description = ''
         new_rep_seq.append(record)
 
-    # 将更新后的ASV表和代表序列文件保存到输出文件
+    # 读取树文件
+    tree = Phylo.read(tree_file, 'newick')
+
+    # 重命名树文件中的ASV号
+    for clade in tree.find_clades():
+        if clade.name in new_index:
+            clade.name = new_index[clade.name]
+
+    # 将更新后的ASV表、代表序列文件和树文件保存到输出文件
     asv_table.to_csv(output_asv_table_file, sep='\t')
     write_single_line_fasta(new_rep_seq, output_rep_seq_file)
+    Phylo.write(tree, output_tree_file, 'newick')
 
-# 设置输入和输出文件的路径
-asv_table_file = '/media/wzy/work/pytest/otu_table.txt'
-rep_seq_file = '/media/wzy/work/pytest/dna-sequences.fasta'
-output_asv_table_file = '/media/wzy/work/pytest/output_asv_table.tsv'
-output_rep_seq_file = '/media/wzy/work/pytest/output_rep_seq.fasta'
+def select_file():
+    filename = filedialog.askopenfilename()
+    return filename
 
-# 调用rename_asv_ids()函数
-rename_asv_ids(asv_table_file, rep_seq_file, output_asv_table_file, output_rep_seq_file)
+def select_directory():
+    directory = filedialog.askdirectory()
+    return directory
+
+def main():
+    root = tk.Tk()
+    asv_table_file = tk.StringVar()
+    rep_seq_file = tk.StringVar()
+    tree_file = tk.StringVar()
+    output_dir = tk.StringVar()
+
+    asv_table_file_button = tk.Button(root, text="选择ASV表文件", command=lambda: asv_table_file.set(select_file()))
+    asv_table_file_button.pack()
+    rep_seq_file_button = tk.Button(root, text="选择代表序列文件", command=lambda: rep_seq_file.set(select_file()))
+    rep_seq_file_button.pack()
+    tree_file_button = tk.Button(root, text="选择树文件", command=lambda: tree_file.set(select_file()))
+    tree_file_button.pack()
+    output_dir_button = tk.Button(root, text="选择输出文件夹", command=lambda: output_dir.set(select_directory()))
+    output_dir_button.pack()
+    run_button = tk.Button(root, text="运行", command=lambda: rename_asv_ids(asv_table_file.get(), rep_seq_file.get(), tree_file.get(), f"{output_dir.get()}/output_asv_table.tsv", f"{output_dir.get()}/output_rep_seq.fasta", f"{output_dir.get()}/output_tree.nwk"))
+    run_button.pack()
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
